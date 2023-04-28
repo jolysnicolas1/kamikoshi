df0ff6ef08344af0d876395f6432e501900d9538
nicolas jolys <jolys.nicolas@gmail.com>
2023-04-28 21:15:00 +0200

    Change colors

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,10 @@ const noto = Noto_Sans_JP({
 })
 export default function Home() {
   return (
-    <main className='flex min-h-screen max-h-screen flex-col items-center justify-between drop-shadow-lg relative bg-white pt-24'>
-      <h1 className="absolute top-[20%] left-[20%] text-[72px] text-red-100 drop-shadow-md { noto.className }">
-        Kamikoshi studio
-      </h1>
+    <main className='flex min-h-screen max-h-screen flex-col items-center justify-between drop-shadow-lg relative bg-black pt-24'>
+        <h1 className="absolute top-[20%] left-[20%] text-[72px] text-fuchsia-500 drop-shadow-sm bg-black px-4 { noto.className }">
+          Kamikoshi studio
+        </h1>
       {/* <Avatar /> */}
       <Image
         src={ mountains }
